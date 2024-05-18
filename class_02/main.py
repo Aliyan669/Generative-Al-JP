707ce8af018447c44debfe33dbb8ea34972df4f2
Aliyan Amir <tboy02722@gmail.com>
2024-05-19 04:01:47 +0500

    Python JP

---
+++ class_02/main.py
@@ -0,0 +1,223 @@
+# creat folder
+# open vscode from terminal using the command "code ."
+# create file "main.py"
+# change directory using command "cd"
+# move backward using command "cd .."
+# open folder/directory using command "cd <folder_name>"
+# run python file using command "python <file_name>.py"
+# open integrated terminal in vscode using the menu > terminal -> new terminal
+
+# print("hello world")
+
+# (continue) introducton to varibale
+# RAM vs hardisk (temp memory vs permanent memory)
+# variable address
+# memory leakage (memory control and garbage collector)
+# high level language vs low level language
+# dynamic langugage vs strongly typed langugae
+# x = 1
+# y = "abcd"
+
+
+#  Data Types
+#  int -> Decimal Number -> 1...1000...n
+#  float -> 1.5, 3.142, 60.2
+#  string -> "I live in pakistan", "aliyan", "age"
+#  boolean -> True/False
+#  NoneType -> None -> example "-", "N/A" -> "", 0, "None"
+
+
+# name ="Aliyan" #string
+# age= 18 # int
+# country = "Pakistan" #string
+# have_job = True # bool
+# weight = 44.3 # float
+# favourite_food = "Biryani" #string
+# favourite_place = "Naran" #string
+# salary = None # NoneType
+# long_text = "Lorem Ipsum \n is simply dummy \n text of the printing \n and typesetting industry"
+
+
+# print(name)
+# print(age)
+# print(country)
+# print(weight)
+# print(have_job)
+# print(favourite_food)
+# print(favourite_place)
+# print(salary)
+# print(long_text)
+
+
+# Type Function
+# print(
+#     type(name)
+# )
+
+
+# variable naming convention or variable naming rules
+# favrt_food = ""
+# favrt food = ""
+# favrt-food = ""
+# frvt_food = ""
+
+
+# top_10_items = ""
+# 10_top_items = ""
+# top_items_10 = ""
+# _favrt_fiid = ""
+
+# alpha-numeric characters
+
+# specical_characters = "+-*()^%$#@!."
+
+# x = "10"
+# X = "10"
+
+# variables are case-sensitive
+# both are different variables
+# "x != X"
+
+# variable name should use snake_case (other cases are camelCase, Pascalcase)
+
+# my_first_book = "" # snake case
+# myFirstBook = "" # camelCase
+# Myfirstbook = "" # Pascalcase
+
+
+# long_text = "text 1 \n text 2 \n text3"
+# print(long_text)
+
+
+# long_text = "text 1 \t text 2 \t text3"
+#  print(long_text)
+
+
+# """
+# Welcome to the Python Cafe
+#     1. Coffe        $2.00
+#     2. Tea          $ 1.50
+# """
+
+# print("Welcome to the Python Cafe \n \t 1. Coffee \t $2.00 \n\t 1. Tea \t $1.50 \n")
+
+# input function   User Value Prompt
+# print("hell world")
+
+# line by line execution demo
+# print("hellow world 1")
+# input("write you name")
+# print("hellow world 2")
+
+# input("write your name")
+
+# x = input("write you name")
+# print(x)
+
+
+# findout the input type
+# x = input("write you name \n")
+
+# print(
+#     type(x)
+# )
+
+
+# x = input("write  \n")
+
+# name = input("Write Your Name")
+# age = input("Write Your Age")
+
+# print(name , type(name) ,age ,  type(age))
+
+
+# Concatination or Concat
+
+# area = input("Write Your Area")
+# city = input("Write Your City")
+# country = input("Write Your Country")
+# address = area + "" + city + "" + country +""
+# print(address)
+
+# Convert String to Int 
+
+# num_1 = int( input("Enter the First Number"))
+# num_2 = int( input("Enter the Second Number"))
+
+# display a sum of the 2 numbers;
+
+# num_1 = "10"
+# num_2 = "10"
+
+# print(num_1 + num_2)
+
+# Type casting / type conversion
+# str() 
+# int()
+# float()
+# bool()
+
+# num_1 = int("10")
+# num_2 = int("10")
+
+# print(int(num_1) + int(num_2))
+
+# name = input("Enter Your Name")
+# print("Hi," + name + ", Welcome Back")
+
+
+# num_1 = int( input("Enter the First Number"))
+# num_2 = int( input("Enter the Second Number"))
+
+# print(num_1 + num_2)
+# print(num_1 * num_2)
+# print(num_1 // num_2)
+
+
+# String Methods 
+
+# lower
+# upper
+# capitalize
+# title
+# split
+# replace
+
+
+# method = "Hello World"
+# print(method.lower())
+# print(method.upper())
+# print(method.capitalize())
+# print(method.title())
+# print(method.split(" "))
+# print(method.replace("World","Aliyan"))
+
+
+# Global built-in functions
+# print()
+# len()
+# round()
+# input()
+# isinstance()
+# type()
+# id()
+# str(), int(), float(), bool()
+
+
+# Table User Print 
+
+# num = int(input("What the number of table"))
+# # print(type(num))
+
+# val = int(input("What the table of range"))
+# for i in range(val):
+#     print(num , "x", i+1 , "=", num*(i+1))
+
+
+# If Else  Statement
+
+# num = 3
+# if num == 3:
+#     print("Successfull")
+# else:
+#     print("Error")
