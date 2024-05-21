49eaae6545a1a9f3f5eb0e2e7e283f340d26d293
Aliyan Amir <tboy02722@gmail.com>
2024-05-22 04:08:24 +0500

    Python Assignment

---
+++ Assignments/assignment1.py
@@ -0,0 +1,40 @@
+# Solution 1
+
+# user = float(input("Enter the Float Number "))
+# print(round(user))
+# print("Original Number is " , user ," Round of Number is " , round(user) )
+
+# user = input("Enter the Sentence ")
+# print("Sentence Length is" , len(user))
+
+
+# Solution 2
+
+# user = input("Enter the Sentence ")
+# print("Uppercase ---- " ,user.upper())
+# print("Lowercase ---- " ,user.lower())
+# print("Capitalize ---- " ,user.capitalize())
+# print("Capitalize ---- " ,user.title())
+
+
+# Solution 3
+
+# sentence = "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+# print(sentence)
+# repl_sen = input("what the word you replace")
+# repl_sent = input("write the word on you replace")
+# print(sentence.replace(repl_sen , repl_sent))
+
+
+# Solution 4
+
+# user = int(input("Enter your Age "))
+# print("In 10 years, you will be" , user+10 , "years old. where" , user+1 , "is the new age")
+
+
+# Solution 5
+
+# full_name = input("Enter Your Full Name ")
+# print("Uppercase ---- " ,full_name.upper())
+
+
