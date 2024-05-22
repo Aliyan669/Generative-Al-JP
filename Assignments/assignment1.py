1c528c7b86c1e0fbf70a5c4f2492f87dc63a9473
Aliyan Amir <tboy02722@gmail.com>
2024-05-23 04:11:12 +0500

    Python Assignment

--- Assignments/assignment1.py
+++ Assignments/assignment1.py
@@ -34,7 +34,34 @@
 
 # Solution 5
 
-# full_name = input("Enter Your Full Name ")
+# full_name = input("Enter Your Full Name ")    
 # print("Uppercase ---- " ,full_name.upper())
+# user = int(input("Enter your Favourite Number "))
+# print("Your favorite number multiplied by 2 is " + str(user*2)  + " where " + str(user) + ", is the New number.")
+
+
+# Solution 6
+
+# first_name = input("Enter Your First Name ")    
+# last_name = input("Enter Your Last Name ")  
+# full_name = first_name +" "+ last_name
+# print("Your full name in Uppercase is: " + full_name.upper())
+# print("Your full name in Modified is: " + full_name.replace(" ", "-"))
+# print("Your full name in Length is: " + str(len(full_name)))
+
+
+# Solution 7
+
+# num_one = int(input("Enter your First Number "))
+# num_two = int(input("Enter your Second Number "))
+# total = (num_one + num_two) / 2
+# print("Orignal Average Number is "+ str(total) + " Average Round of Number is "+ str(round(total)))
+
+
+# Solution 8
+
+# sentence = input("Enter a Sentence ")
+# print("Replace all Spaces to Underscores is "+sentence.replace(" ","_"))
+# print(sentence.split(" "))
 
 
