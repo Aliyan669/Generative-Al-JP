8dfc01295cbdb39514ea73aec3025931ad79a80c
Aliyan Amir <tboy02722@gmail.com>
2024-06-01 03:07:16 +0500

    Python Assignment

---
+++ Assignments/assignment2.py
@@ -0,0 +1,81 @@
+# Write a program to check whether a person is eligible to vote or not?
+
+# age = 18
+
+# age = int(input("Please Enter your age "))
+# if age >= 18:
+#    print("You are eligible to vote")
+# else:
+#    print("You are not eligible to vote") 
+
+
+# Write a program to check char is vowel or not. - elif
+
+# vowel chars are
+# 1. a
+# 2. e
+# 3. i
+# 4. o
+# 5. u
+
+# char = input("Enter a Vowel Number ")
+
+# if char == "a":
+#     print("Vowel")
+# elif char == "e":
+#     print("Vowel")
+# elif char == "i":
+#     print("Vowel")
+# elif char == "o":
+#     print("Vowel")
+# elif char == "u":
+#     print("Vowel")
+# else:
+#     print("Not Vowel")
+
+
+# Second Method
+
+# char = input("Enter a Vowel Number ")
+
+# if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
+#     print("Vowel")
+# else:
+#     print("Not Vowel")
+
+
+# Write a program to check the number is positive or negative. User input.
+
+# a = int(input("Please Enter a number "))
+# if a > 0:
+#     print("Number is Positive ")
+# else:
+#      print("Number is Negative ") 
+
+
+# Write a program to check whether a number is odd or even? Use Modulus Operator
+
+# a = 8
+
+# a = int(input("Please Enter a Number "))
+# if a % 2 == 0:
+#     print("Even Number")
+# else:
+#     print("Odd Number")
+
+
+# Write a program to display the grade of the user in subject A, ask user marks obtained out of 100
+
+# marks = 90
+
+# marks = int(input("Please Enter Obtained Marks"))
+# if marks >= 80:
+#     print("A+")
+# elif marks >= 70:
+#     print("A")
+# elif marks >= 60:
+#     print("B")
+# elif marks >= 50:
+#     print("C")
+# else:
+#     print("no grading")
