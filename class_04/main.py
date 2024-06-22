c1758b5727c8b235969e0fc68c51c93dea6229c0
Aliyan Amir <tboy02722@gmail.com>
2024-06-22 23:42:02 +0500

    Python JP

---
+++ class_04/main.py
@@ -0,0 +1,144 @@
+# GithubSetup
+
+# create account on github
+# https://github.com/signup
+
+# login to github
+# https://github.com/login
+
+# open profile
+# open repositories
+# create new repository with readme file on github
+# update readme file
+
+# see all commit history
+# commit detail
+# hash
+# author
+# date and time
+  
+  
+  
+  
+# Github Local Setup 
+
+# install git: https://git-scm.com/
+# https://youtu.be/Ez8F0nW6S-w?t=1129
+
+# copy remote repository
+# git clone <url>
+
+# made changes in the file and commit
+# git add .
+# git status
+# git commit -m "this is my message"
+# git push
+  
+  
+  
+  
+# Git Copy Remote Repository
+  
+# git clone <url>
+# cd your_project_name
+
+# show status (track/untrack/modified/staged file) of the working directory
+# git status
+
+# made changes in the file and commit
+# git add .
+# git commit -m "this is my message"
+# git push
+
+
+
+
+# Github Branch Command
+
+# Create
+# git checkout -b "feature-branch"
+
+# Read
+# git branch # list all branches locally
+
+# Delete
+# git branch -D "feature-branch"
+
+# change branch
+# git checkout 
+
+
+
+
+# Github Undo Command
+
+# Git reset <filename>
+# Git reset HEAD~1 # by default soft delete
+
+# git reset --soft HEAD~5
+# Effect: Moves HEAD to the commit 5 steps back but does not change the working directory or the index.
+# Result: All changes from the last 5 commits will be staged. The working directory will still reflect the most recent state before the reset, but all changes from those 5 commits will be in the staging area.
+# Git reset –hard HEAD~1
+
+# undo git reset command
+# git reflog # git running command history
+# git reset "HEAD@{num}" 
+
+
+
+
+# Github Vocabulary
+
+# Git status
+# - Untrack
+# - Modified
+# - unmodified
+# - Staged
+
+# Git: version control system that tracks versions of files
+
+# version control: tracking and managing changes of files
+
+# Github: create, store, manage and share their code.
+
+# Local repo or repository: A local directory containing code and files for the project
+
+# Remote repository: An online version of the local repository hosted on services like GitHub, GitLab, and BitBucket
+
+# Cloning: The act of making a clone or copy of a repository in a new directory
+
+# Staging area: a cache that holds changes you want to commit next.
+
+# Commit: A snapshot of the project you can come back to
+
+# Commit ID or hash: a unique identifier for each commit, used for switching to different save points.
+
+# Head: last commit is by default head
+
+# .git (folder): git uses this folder for tracking files
+
+# Branch: A copy of the project used for working in an isolated environment without affecting the main project
+
+# Git merge: The process of combining two branches together
+
+# Merge Conflicts: Merge conflicts occur when changes are made to the same line of a file
+
+
+
+
+# Github Tutorials 
+
+# youtube videos for github
+# https://www.youtube.com/watch?v=osNNbE3molA
+# https://www.youtube.com/watch?v=Ez8F0nW6S-w
+# https://www.youtube.com/watch?v=tz82ola3oy0
+
+
+# cheetsheet for github comands
+# https://education.github.com/git-cheat-sheet-education.pdf
+# https://www.datacamp.com/cheat-sheet/git-cheat-sheet
+
+
+# MarkDown file (i.e readme.md) styling guide
+# https://www.geeksforgeeks.org/what-is-readme-md-file/
+# https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
