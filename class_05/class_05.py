f860b40e7daf43b94b3c80f7d545d5d467538aa1
Aliyan Amir <tboy02722@gmail.com>
2024-06-23 20:37:58 +0500

    Python JP

--- class_05/class_05.py
+++ class_05/class_05.py
@@ -36,7 +36,6 @@
 # print(fruits[3]) # It will raise error "Index out of Range"
 
 
-
 # Find the length of list using     len()
  
 # fruits = ["Apple","Mango", "Banana"]
@@ -53,7 +52,6 @@
 # print(fruits[-1])
 
 
-
 # Update List 
 
 # fruits = ["Apple","Mango", "Banana"]
@@ -62,14 +60,12 @@
 # print(fruits)
 
 
-
 # Delete element on List using     del()
 
 # fruits = ["Apple","Mango", "Banana"]
 
 # del fruits[0]
 # print(fruits)
-
 
 
 # Slice
@@ -86,7 +82,6 @@
 # print(last_element)
 
 
-
 # Add New Element on List using       append()
 
 # fruits = ["Apple","Mango", "Banana"]
@@ -95,12 +90,10 @@
 # print(fruits)
 
 
-
 # Remove Last Element on List using      pop()
 
 # fruits.pop()
 # print(fruits)
-
 
 
 # Extend on List using      extend()
@@ -109,18 +102,15 @@
 # list_2.extend(fruits)
 # print(list_2)
 
-
 # list_3 = ["Aliyan", "Amir", "imran", "Nihal"]
 # list_3.extend(list_2)
 # print(list_3)
-
 
 # print("list_3", list_3)
 # print('list_2', list_2)
 
 # list_2 = list_2 + list_3
 # print(list_2)
-
 
 
 # Sorting on List using      sort() 
@@ -135,12 +125,10 @@
 # print(names_list)
 
 
-
 # Find Elemnet Index Number by Value on List using      index()
 
 # names_list = ["Aliyan", "Akif","Javed","Basit","Tahir","Nihal"]
 # print("find by value:", names_list.index("Javed"))
-
 
 
 # Remove Element by Value on List using      remove()
@@ -148,7 +136,6 @@
 # names_list = ["Aliyan", "Akif","Javed","Basit","Tahir","Nihal"]
 # names_list.remove("Javed")
 # print(names_list)
-
 
 
 # Remove All Element on List using      Clear()
@@ -160,12 +147,10 @@
 # print(names_list)
 
 
-
 # Count Duplicate on List  using        count()
 
 # names_list = ["Aliyan", "Akif","Javed","Basit","Tahir","Nihal","Akif"]
 # print("Count Duplicate", names_list.count("Akif"))
-
 
 
 # Insert at Defined Position using       insert()
@@ -175,14 +160,12 @@
 # print(names_list)
 
 
-
 # Class Task
 
 # create a list of juices, add 5 items using append
 # creat a list of cars, add 3 items using insert
 # remove last time from the list ["bed", "table", "chair", "sofa"] using pop and len
 # remove the item "chair" from the list ["bed", "table", "chair", "sofa"] using del and remove
-
 
 
 # create a list of juices, add 5 items using append
@@ -198,7 +181,6 @@
 # print(juices)
 
 
-
 # creat a list of cars, add 3 items using insert
 
 # cars = []
@@ -209,7 +191,6 @@
 # print(cars)
 
 
-
 # Remove last time from the list ["bed", "table", "chair", "sofa"] using pop and len
 
 # furniture = ["bed", "table", "chair", "sofa"]
@@ -218,7 +199,6 @@
 
 # del furniture[len(furniture) - 1]
 # print(furniture)
-
 
 
 # remove the item "chair" from the list ["bed", "table", "chair", "sofa"] using del and remove
@@ -233,27 +213,25 @@
 # print(furniture)
 
 
-
 # Slice Element Check using   (in)    and return  True/False
  
 # furniture = ["bed", "chair", "table"]
 # print("pepsi" in furniture)
-
 
 # value in list
 
 # print("bed" in furniture)
 
 
-
  # "C" != "c" # case sensitive
  
  
- 
 # slice
+
 # syntax: varibale[start_from : till]
 
 # Exclusive vs Inclusive
+
 
 # car_list = ["corolla", "mehran", "cultus", "aqua", "civic", "vitz", "alto"]
 # print(car_list)
@@ -278,7 +256,6 @@
 # print("--".join(detail_list))
 
 
-
 # Loop on List 
 
 # names_list = ["Aliyan", "Akif","Javed","Basit","Tahir","Nihal"]
@@ -291,28 +268,23 @@
 # for i in ["Aliyan", "Akif","Javed","Basit","Tahir","Nihal"]:
 #     print(i)
 
-
 # names_list = ["Aliyan", "Akif","Javed","Basit","Tahir","Nihal"]
 
 # for i in names_list:
 #     print(i)
-
 
 # print(list(range(10)))
 
 # print(range(len(names_list)))
 
 
-
 # for item in ["Aliyan", "Akif","Javed","Basit","Tahir","Nihal"]:
 #     print(item)
-
 
 
 # names_list = ["Aliyan", "Akif","Javed","Basit","Tahir","Nihal"]
 # for i in range(len(names_list)): # [0, 1, 2, 3, 4, 5]
 #     print(names_list[i])
-
 
 
 # loop over Furniture
